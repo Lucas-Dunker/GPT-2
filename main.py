0df501e01f957410975909361be165e596747761
Lucas Dunker <ldunker2003@gmail.com>
2026-03-02 22:01:10 -0500

    Step 2: Building a Data Loader

--- main.py
+++ main.py
@@ -20,3 +20,47 @@ def decode(input_tokens: list[int]) -> str:
 
 # convert our text data into tokenized tensor
 data = torch.tensor(encode(text), dtype=torch.long, device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"))
+
+train_batch_size = 16  # training batch size
+eval_batch_size = 8  # evaluation batch size
+context_length = 256  # number of tokens processed in a single batch
+train_split = 0.8  # percentage of data to use from total data for training
+
+# split data into trian and eval
+n_data = len(data)
+train_data = data[:int(n_data * train_split)]
+eval_data = data[int(n_data * train_split):]
+
+
+class DataLoader:
+    def __init__(self, tokens, batch_size, context_length) -> None:
+        self.tokens = tokens
+        self.batch_size = batch_size
+        self.context_length = context_length
+
+        self.current_position = 0
+
+    def get_batch(self) -> torch.tensor:
+        b, c = self.batch_size, self.context_length
+
+        start_pos = self.current_position
+        end_pos = self.current_position + b * c + 1
+
+        # if the batch exceeds total length, get the data till last token
+        # and take remaining from starting token to avoid always excluding some data
+        add_data = -1 # n, if length exceeds and we need `n` additional tokens from start
+        if end_pos > len(self.tokens):
+            add_data = end_pos - len(self.tokens) - 1
+            end_pos = len(self.tokens) - 1
+
+        d = self.tokens[start_pos:end_pos]
+        if add_data != -1:
+            d = torch.cat([d, self.tokens[:add_data]])
+        x = (d[:-1]).view(b, c)  # inputs
+        y = (d[1:]).view(b, c)  # targets
+
+        self.current_position += b * c # set the next position
+        return x, y
+
+train_loader = DataLoader(train_data, train_batch_size, context_length)
+eval_loader = DataLoader(eval_data, eval_batch_size, context_length)
